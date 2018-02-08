58370786e62d7937694ac75dbcc4e166f3c209b0
bermuda-z <bermuda.mew@gmail.com>
2018-02-08 22:25:49 +0700

    Update FillOchar.asm
    all comment

--- FillOchar.asm
+++ FillOchar.asm
@@ -37,8 +37,7 @@ printOT:
 	int 10h
 	jmp checkyT
 
-
-checkyT:	; odd or even?
+checkyT:	; row(y) odd or even?
 	test dh,1h    		
 	jz evenT		; jump if row(y) is even	
 
@@ -62,8 +61,8 @@ nextevenT:	; newline is even
 	jmp printOT		; jump to printing 'o'
 
 nextoddT:	; newline is odd
-	cmp dh,18h		; compare row(y) is greater or equal 24	
-	jge clearT		; jump to clearscreen if row(y)	is greater or equal 24	
+	cmp dh,18h		; compare row(y) is greater than or equal 24	
+	jge clearT		; jump to clearscreen if row(y)	is greater than or equal 24	
 	inc dh			; increase one row(y)
 	dec dl			; decrease one column(x)
 	jmp printOT		; jump to printing 'o'
@@ -101,7 +100,7 @@ printOD:
 	; pop current cursor position to stack
 	pop dx
 	
-	;printO
+	; printO
 	mov ah,9h		; set to write character mode
 	mov cx,1		; print 1 character
 	mov bl,3h		; set color of character
@@ -109,7 +108,7 @@ printOD:
 	int 10h
 	jmp checkyD
 
-checkyD:	; odd or even?
+checkyD:	; row(y) odd or even?
 	test dh,1h    
 	jz evenD		; jump if row(y) is even
 
@@ -148,108 +147,126 @@ clearD:		; clearscreen
 	jmp LtoR
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 LtoR:
-	mov dh , 18h  ;y
-	mov dl , 0h ;x
+	; set start position
+	mov dh , 18h  ;row(y) is 24 (0 - 24)
+	mov dl , 0h ;column(x) is 0 (0 - 79)
 	mov bh , 0h
 printOL:
+	; move cursor
 	mov ah, 2h
 	int 10h
 	
+	; push current cursor position to stack
 	push dx
-	
+
+	; delay 1000 ms
 	mov ah,86h
 	mov cx,0000h
 	mov dx,2710h
 	int 15h
 
+	; pop current cursor position to stack
 	pop dx
 	
-	mov ah,9h
-	mov cx,1
-	mov bl,9h
-	mov al,6Fh
+	; printO	
+	mov ah,9h		; set to write character mode
+	mov cx,1		; print 1 character
+	mov bl,9h		; set color of character
+	mov al,6Fh		; set character 'o'
 	int 10h
 	jmp checkxL
-checkxL:
+	
+checkxL:	; column is odd or even?
 	test dl,1h          
-	jz evenL		
-oddL:	
-	inc dh		
-	cmp dh,19h
-	je nextevenL
-	jmp printOL
-evenL:	
-	dec dh		
-	cmp dh,0h
-	js nextoddL
-	jmp printOL
-nextoddL:		
-	inc dl
-	inc dh 
-	jmp printOL
-nextevenL:
-	cmp dl,4Fh
-	jge clearL		
-	inc dl
-	dec dh
-	jmp printOL
+	jz evenL		; jump if column(x) is even		
+oddL:		; column(x) is odd		
+	inc dh			; increase one row(y)	
+	cmp dh,19h		; compare row(y) is equal 24
+	je nextevenL		; jump to newlineeven if row(y) is equal 24
+	jmp printOL		; jump to printing 'o'
+evenL:		; column(x) is even	
+	dec dh			; decrease one row(y)		
+	cmp dh,0h		; compare row(y) is less than 0 
+	js nextoddL		; jump to newlineodd if row(y) less than 0
+	jmp printOL		; jump to printing 'o'
+nextoddL:	; newline is odd		
+	inc dl			; increase one column(x)
+	inc dh			; increase one row(y)
+	jmp printOL		; jump to printing 'o'
+nextevenL:	; newline is even
+	cmp dl,4Fh		; compare column(x) is greater than or equal 80
+	jge clearL		; jump to clearscreen if column(x) is greater than or equal 80	
+	inc dl			; increase one column(x)
+	dec dh			; decrease one row(y)
+	jmp printOL		; jump to printing 'o'
 
-clearL:	
+clearL:		; clearscreen
 	mov ah,6h
-	mov al,0h
+	mov al,0h		; clear whole screen
 	mov bh,7h
 	mov cx,0h
 	mov dx,184fh
 	int 10h
 	jmp RtoL
+	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 RtoL:
-	mov dh , 0h  ;y
-	mov dl , 4Fh ;x
+	; set start position 
+	mov dh , 0h  ; row(y) is 0 (0 - 24)
+	mov dl , 4Fh ; column(x) is 79 (0 - 79)
 	mov bh , 0h
+	
 printOR:
+	; move cursor
 	mov ah, 2h
 	int 10h
 	
-	push dx	
+	; push current cursor position to stack
+	push dx
+	
+	; delay 1000 ms
 	mov ah,86h
 	mov cx,0000h
 	mov dx,2710h
 	int 15h
+	; pop current cursor position to stack
 	pop dx
-
-	mov ah,9h
-	mov cx,1
-	mov bl,1h
-	mov al,6Fh
+	
+	; printO
+	mov ah,9h		; set to write character mode
+	mov cx,1		; print 1 character
+	mov bl,1h		; set color of character
+	mov al,6Fh		; set character 'o'
 	int 10h
 	jmp checkxR
-checkxR:
+	
+checkxR:	; column is odd or even?
 	test dl,1h          
-	jz evenR	
-oddR:	
-	inc dh		
-	cmp dh,19h
-	je nextevenR
-	jmp printOR
-evenR:	
-	dec dh		
-	cmp dh,0h
-	js nextoddR
-	jmp printOR
-nextoddR:
-	cmp dl,0h
-	jle exit	
-	dec dl
-	dec dh 
-	jmp printOR
-nextevenR:		
-	dec dl
-	dec dh
-	jmp printOR
-exit:	
+	jz evenR		; jump if column(x) is even
+oddR:		; column(x) is odd	
+	inc dh			; increase one row(y)	
+	cmp dh,19h		; compare row(y) is equal 24
+	je nextevenR		; jump to newlineeven if row(y) is equal 24	
+	jmp printOR		; jump to printing 'o'
+evenR:		; column(x) is even
+	dec dh			; decrease one row(y)	
+	cmp dh,0h		; compare row(y) is less than 0
+	js nextoddR		; jump to newlineodd if row(y) is less than 0
+	jmp printOR		; jump to printing 'o'
+nextoddR:	; newline is odd
+	cmp dl,0h		; compare column(x) is less than or equal 0
+	jle exit		; jump to exit and clearscreen if column(x) is less than or equal 0
+	dec dl			; decrease one column(x)
+	dec dh			; decrease one row(y)
+	jmp printOR		; jump to printing 'o'
+nextevenR:	; newline is even		
+	dec dl			; decrease one column(x)
+	dec dh			; decrease one row(y)
+	jmp printOR		; jump to printing 'o'
+exit:		; exit and clearscreen	
 	mov ah,6h
-	mov al,0h
+	mov al,0h		; clear whole screen
 	mov bh,7h
 	mov cx,0h
 	mov dx,184fh
